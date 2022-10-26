0924247a605f1681708597e1041b3cde429f5cb4
John Titor <foolycoolyfly@zoho.com>
2022-10-26 18:57:57 +0300

    upd: improved tool

--- src/bin/tool/main.rs
+++ src/bin/tool/main.rs
@@ -8,6 +8,7 @@ use env_logger::Env;
 pub mod create_net;
 pub mod dataset_info;
 pub mod train;
+pub mod test;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
@@ -87,7 +88,42 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .arg(Arg::new("WriteErrToFile").long("err_to_file").help(
             "Can be true or false, if true test network error will be recorded to file err.log",
         ).action(ArgAction::Set).require_equals(true)))
-        .subcommand(Command::new("dataset_info").about("Inspect dataset"))
+        .subcommand(Command::new("test").about("Test net").arg(
+            Arg::new("Data")
+            .long("dataset")
+            .short('d')
+            .takes_value(true)
+            .require_equals(true)
+            .required(true)
+        )
+        .arg(Arg::new("SolverCfgYaml")
+                .long("solver_cfg_yaml")
+                .help("Provide solver configuration")
+                .required(true)
+                .takes_value(true)
+                .require_equals(true))
+        .arg(Arg::new("SolverState")
+                .short('s')
+                .long("state")
+                .help("Provide solver state. Weights state to start training")
+                .action(ArgAction::Set)
+                .takes_value(true)
+                .require_equals(true))
+        .arg(Arg::new("TestBatch")
+                .long("test_batch")
+                .short('b')
+                .required(true)
+                .require_equals(true)
+                .takes_value(true)
+                .value_parser(clap::value_parser!(usize))
+        ))
+        .subcommand(Command::new("dataset_info").about("Inspect dataset").arg(
+            Arg::new("Data")
+            .long("dataset")
+            .short('d')
+            .takes_value(true)
+            .require_equals(true)
+        ))
         .subcommand(Command::new("create_net").about("Create a new net configuration").arg(
             Arg::new("OutFile")
                 .long("out")
@@ -102,11 +138,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cmd = matches.subcommand().unwrap();
 
     if cmd.0 == "dataset_info" {
-        dataset_info::dataset_info(&matches)?;
+        let (_subcmd, args) = matches.subcommand().unwrap();
+        dataset_info::dataset_info(&args)?;
     }
     if cmd.0 == "train" {
         let (_subcmd, args) = matches.subcommand().unwrap();
         train::train_new(&args)?;
+    }
+    if cmd.0 == "test" {
+        let (_subcmd, args) = matches.subcommand().unwrap();
+        test::test_net(&args)?;
     }
     if cmd.0 == "create_net" {
         let (_subcmd, args) = matches.subcommand().unwrap();
