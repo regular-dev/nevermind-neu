da717300eea284efb2148c51f506e5335d17202b
John Titor <foolycoolyfly@zoho.com>
2022-12-16 17:45:55 +0300

    fix: test tool

--- src/bin/tool/test.rs
+++ src/bin/tool/test.rs
@@ -17,16 +17,20 @@ pub fn test_net(
     let mut model = Sequential::from_file(&model_cfg)?;
 
     let model_state_file = args.get_one::<String>("ModelState").unwrap();
-    model.load_state(&model_state_file);
+    model.load_state(&model_state_file)?;
 
     let ds_path = args.get_one::<String>("Data").unwrap();
     let test_data = Box::new(ProtobufDataLoader::from_file(ds_path)?);
 
     let test_batch = args.get_one::<usize>("TestBatch").unwrap();
 
+    model.set_batch_size(1);
+
     let mut net = Network::new_for_eval(model);
 
+    // let mut counter = 0;
     // TODO : check label impl
+    // TODO : add features to display accuracy of testing
     for i in 0..*test_batch {
         info!("Test {} , evaluating", i);
         let test_batch = test_data.next_batch(1);
@@ -40,7 +44,10 @@ pub fn test_net(
         }
 
         info!("Below label is : {}", label);
-        net.eval(test_batch.input);
+        let out = net.eval(test_batch.input);
+        let out_b = out.borrow();
+        info!("Neural output is : {}", out_b);
+        info!("==========")
     }
 
     Ok(())
