252a9fb22307ce30903b6da5aadcd3d3ad9153ac
John Titor <foolycoolyfly@zoho.com>
2023-02-20 18:11:52 +0300

    upd: commented unnecesary linked libs

--- build.rs
+++ build.rs
@@ -4,11 +4,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     prost_build::compile_protos(&["src/solvers.proto"],
                                 &["src/"])?;
                                 
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rustc-link-lib={}=openblas", "dylib");
-    println!("cargo:rustc-link-lib={}=cblas", "dylib");
-
-    println!("caceshka");
+    // println!("cargo:rerun-if-changed=build.rs");
+    // println!("cargo:rustc-link-lib={}=openblas", "dylib");
+    // println!("cargo:rustc-link-lib={}=cblas", "dylib");
 
     Ok(())
 }
