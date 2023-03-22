4abfe49702e9bdb769939fff5f7ac88d1a4575d6
John Titor <foolycoolyfly@zoho.com>
2023-03-22 18:33:24 +0300

    upd: xor_ocl

--- examples/xor_ocl/main.rs
+++ examples/xor_ocl/main.rs
@@ -3,11 +3,10 @@ use log::info;
 use std::time::Instant;
 
 use env_logger::Env;
-
-use ndarray_rand::rand::thread_rng;
+use ndarray::array;
 
 use nevermind_neu::dataloader::*;
-use nevermind_neu::network::*;
+use nevermind_neu::orchestra::*;
 use nevermind_neu::models::*;
 use nevermind_neu::optimizers::*;
 
@@ -27,48 +26,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dataset_train.push(LabeledEntry::new(vec![1.0, 0.0], vec![1.0]));
     dataset_train.push(LabeledEntry::new(vec![1.0, 1.0], vec![0.0]));
 
-    let mut dataloader = Box::new(SimpleDataLoader::new(dataset_train));
-
-    //let mut solver_rms = SolverRMS::from_file("network.cfg")?;
-    // solver_rms.load_state("solver_state.proto")?;
+    let dataloader = Box::new(SimpleDataLoader::new(dataset_train));
 
     let net_cfg = vec![2, 10, 1];
     let mut seq_mdl = SequentialOcl::new_simple(&net_cfg);
     seq_mdl.set_batch_size(4);
 
-    let mut opt = Box::new(OptimizerOclRms::new(seq_mdl.queue()));
+    let opt = Box::new(OptimizerOclRms::new(seq_mdl.queue()));
 
-    //let mut net = Network::new(seq_mdl).test_batch_num(4);
+    seq_mdl.set_optim(opt);
 
-    //net.set_optimizer(opt);
-    //net.set_train_dataset(dataloader);
+    let mut net = Orchestra::new(seq_mdl).test_batch_size(4);
 
-    //  net.save_network_cfg("network.cfg")?;
-    for _ in 0..1500 {
-      let t_data = dataloader.next_batch(4);
-      seq_mdl.feedforward(t_data.input);
-      seq_mdl.backpropagate(t_data.output);
-      seq_mdl.optimize(&mut opt);
-    }
-
-    return Ok(());
+    net.set_save_on_finish_flag(false);
+    net.set_train_dataset(dataloader);
 
     let now_time = Instant::now();
 
-  //  net.train_for_error_or_iter(0.05, 2000)?;
+    net.train_for_error_or_iter(0.05, 2000)?;
 
     let elapsed_bench = now_time.elapsed();
 
     info!("Elapsed for training : {} ms", elapsed_bench.as_millis());
 
-    //net.save_solver_state("solver_state.proto")?;
-
     info!("Now testing net !!!");
 
-  //  let out = net.eval(array![[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]).unwrap();
-   // let out_b = out.borrow();
+    net.update_test_model();
 
-  //  info!("Trained-net XOR out : {}", out_b);
+    let out = net.eval(array![[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]).unwrap();
+    let out_b = out.borrow();
+
+    info!("Trained-net XOR out : {}", out_b);
 
     Ok(())
 }
