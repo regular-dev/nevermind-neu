98608977b43d609b787023b3f4f32fc258abd3aa
John Titor <foolycoolyfly@zoho.com>
2023-04-19 18:18:40 +0300

    add: minibatch constructor

--- src/dataloader/databatch.rs
+++ src/dataloader/databatch.rs
@@ -46,4 +46,27 @@ impl MiniBatch {
             output: out_arr,
         }
     }
+
+    pub fn new_no_ref(b: Vec<LabeledEntry>) -> Self {
+        // TODO : refactor cause dublicating constructors
+        assert!( !b.is_empty() );
+
+        let mut inp_arr = Array2D::zeros( (b.len(), b.first().unwrap().input.shape()[0]) );
+        let mut out_arr = Array2D::zeros( (b.len(), b.first().unwrap().expected.shape()[0]) );
+
+        // Copies memory into batch
+
+        for (idx, it) in b.iter().enumerate() {
+            let mut inp_entry = inp_arr.index_axis_mut(Axis(0), idx);
+            inp_entry.assign(&it.input);
+
+            let mut out_entry = out_arr.index_axis_mut(Axis(0), idx);
+            out_entry.assign(&it.expected);
+        }
+
+        Self {
+            input: inp_arr,
+            output: out_arr,
+        }
+    }
 }
